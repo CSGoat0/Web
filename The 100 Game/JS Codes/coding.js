d60587dc7c0898430a6573da1de781ab1e74cc78
csgoat0 <elturkabdelrahman@gmain.com>
2025-06-15 23:01:49 +0300

    the 100 game/full game except rules button

--- The 100 Game/JS Codes/coding.js
+++ The 100 Game/JS Codes/coding.js
@@ -1,6 +1,9 @@
 //main variables
 let winner = 0;
 let score = 0;
+let timer = 40;
+let p1 = true;
+let p2 = false;
 
 //start game functionality
 function isStarted() {
@@ -21,43 +24,125 @@ strtbtn.onclick = function () {
 }
 
 //score increment
-let numbers = document.getElementById("p1_numbers").querySelectorAll(".numbers");
-numbers.forEach((value, key, parent) => {
-    value.onclick = function () {
-        score += +value.value;
-        console.log(score);
-    }
-});
-
-//Set Turnes
-let p1 = true;
-let p2 = false;
+const scoreCounter = document.getElementById("score_counter");
 let display_turn = document.getElementById("turn");
 let turn_span = document.querySelector("#turn span");
-if (p1) {
-    display_turn.style.justifyContent = "left";
-    turn_span.style = "\
-    background: crimson;\
-    border: 2px solid black;\
-    padding: 5px;\
-    margin: 2px;\
-    border-top-left-radius: 0px;\
-    border-top-right-radius: 20px;\
-    "
-} else if (p2) {
-    display_turn.style.justifyContent = "right";
-    turn_span.style = "\
-    background: crimson;\
-    border: 2px solid black;\
-    padding: 5px;\
-    margin: 2px;\
-    border-top-right-radius: 0px;\
-    border-top-left-radius: 20px;\
-    "
-}
+
+document.getElementById("p1_numbers").querySelectorAll(".numbers").forEach(button => {
+    button.addEventListener("click", () => {
+        if (p1 && !winner) {
+            score += +button.value;
+            p1 = false;
+            p2 = true;
+            scoreCounter.textContent = score;
+        }
+        if (score >= 100) {
+            if (p1) {
+                winner = 2;
+            } else {
+                winner = 1;
+            }
+            clearInterval(id);
+        }
+        if (timer === 0) {
+            if (p1) {
+                winner = 2;
+            } else {
+                winner = 1;
+            }
+            clearInterval(id);
+        }
+        if (winner) {
+            let displayWinner = document.getElementById("score");
+            let span = document.createElement("span");
+            span.textContent = `Player ${winner} Wins`;
+            displayWinner.innerHTML = "";
+            displayWinner.append(span);
+            display_turn.style.display = "none";
+        }
+        if (p1) {
+            display_turn.style.justifyContent = "left";
+            turn_span.style = "\
+            background: crimson;\
+            border: 2px solid black;\
+            padding: 5px;\
+            margin: 2px;\
+            border-top-left-radius: 0px;\
+            border-top-right-radius: 20px;\
+            "
+        } else if (p2) {
+            display_turn.style.justifyContent = "right";
+            turn_span.style = "\
+            background: crimson;\
+            border: 2px solid black;\
+            padding: 5px;\
+            margin: 2px;\
+            border-top-right-radius: 0px;\
+            border-top-left-radius: 20px;\
+            "
+        }
+    });
+});
+
+document.getElementById("p2_numbers").querySelectorAll(".numbers").forEach(button => {
+    button.addEventListener("click", () => {
+        if (p2 && !winner) {
+            score += +button.value;
+            p2 = false;
+            p1 = true;
+            scoreCounter.textContent = score;
+        }
+        if (score >= 100) {
+            if (p1) {
+                winner = 2;
+            } else {
+                winner = 1;
+            }
+            clearInterval(id);
+        }
+        if (timer === 0) {
+            if (p1) {
+                winner = 2;
+            } else {
+                winner = 1;
+            }
+            clearInterval(id);
+        }
+        if (winner) {
+            let displayWinner = document.getElementById("score");
+            let span = document.createElement("span");
+            span.textContent = `Player ${winner} Wins`;
+            displayWinner.innerHTML = "";
+            displayWinner.append(span);
+            display_turn.style.display = "none";
+        }
+        if (p1) {
+            display_turn.style.justifyContent = "left";
+            turn_span.style = "\
+            background: crimson;\
+            border: 2px solid black;\
+            padding: 5px;\
+            margin: 2px;\
+            border-top-left-radius: 0px;\
+            border-top-right-radius: 20px;\
+            "
+        } else if (p2) {
+            display_turn.style.justifyContent = "right";
+            turn_span.style = "\
+            background: crimson;\
+            border: 2px solid black;\
+            padding: 5px;\
+            margin: 2px;\
+            border-top-right-radius: 0px;\
+            border-top-left-radius: 20px;\
+            "
+        }
+    });
+});
+
+scoreCounter.textContent = score;
 
 //Timer Functionality
-let timer = 40;
 let timerRenderer = document.getElementById("timer_circle");
 let timerspan = document.createElement("span");
 timerspan.textContent = timer;
@@ -68,15 +153,7 @@ let id = setInterval(() => {
             timer--;
             timerspan.textContent = timer;
         }
-        else {
-            if (p1) {
-                winner = 2;
-            } else {
-                winner = 1;
-            }
-            clearInterval(id);
-        }
-        if (score >= 100) {
+        if (timer === 0) {
             if (p1) {
                 winner = 2;
             } else {
@@ -93,4 +170,10 @@ let id = setInterval(() => {
             display_turn.style.display = "none";
         }
     }
-}, 100, timer);
+}, 2000, timer);
+
+//To Restart the game
+let restart = document.getElementById("restart");
+restart.onclick = function () {
+    history.go(0);
+}
