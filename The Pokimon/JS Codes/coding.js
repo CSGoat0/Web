aec2c0c75fcc31ba4a642b4fe8e5fe0e8a5f4ccc
csgoat0 <elturkabdelrahman@gmain.com>
2025-06-23 06:04:01 +0300

    The Pokimon refactor

--- The Pokimon/JS Codes/coding.js
+++ The Pokimon/JS Codes/coding.js
@@ -1,5 +1,16 @@
-// create snake class
+// create Pokimon class
 class Pokimon {
+    constructor(board, currentPosition, headSymbol, untiSpeed = 300) {
+        this.board = board;
+        this.current = currentPosition;
+        this.headSymbol = headSymbol;
+        this.untiSpeed = untiSpeed;
+    }
+    score = -1;
+}
+
+
+class MovementManager {
     //to check wither the apple is in my range or not, so i know where to move
     #checkRange(index1, index2) {
         for (let [x, y] of this.#ranges) {
@@ -13,6 +24,8 @@ class Pokimon {
         return false;
     }
 
+
+    //to check boarders
     #maxRightBoarder(number) {
         if (!((number + 1) % 28) && number !== 0) {
             return true;
@@ -41,47 +54,25 @@ class Pokimon {
         else return false;
     }
 
-    #getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+    constructor(pokimon) {
+        this.pokimon = pokimon;
     }
 
-    #checkFood() {
-        for (let element of this.board) {
-            if (element.textContent !== "" && element.textContent !== `${this.headSymbol}`) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    constructor(board, currentPosition, headSymbol, bodySymbol, untiSpeed = 1000) {
-        this.board = board;
-        this.current = currentPosition;
-        this.headSymbol = headSymbol;
-        this.bodySymbol = bodySymbol;
-        this.untiSpeed = untiSpeed;
-    }
-
+    //directions
     up = false;
     right = true;
     down = false;
     left = false;
-    score = -1;
     endGame = false;
 
-    //to check wither the apple is in my range or not, so i know where to move
-    #ranges = [[0, 27], [28, 55], [56, 83], [85, 111], [112, 139], [140, 167], [168, 195], [196, 223], [224, 251],
-    [216, 279], [280, 307], [308, 335], [336, 363], [364, 391], [392, 419], [420, 447], [448, 475], [476, 503],
-    [504, 531], [532, 559]];
-
-    //initiate snake movement
+    //initiate Pokimon movement
     IMove() {
         let interval = setInterval(() => {
             try {
                 if (this.up) {
-                    if (!this.#maxTopBoarder(this.current)) {
-                        this.board[this.current].innerHTML = "";
-                        this.board[this.current -= 28].innerHTML = this.headSymbol;
+                    if (!this.#maxTopBoarder(this.pokimon.current)) {
+                        this.pokimon.board[this.pokimon.current].textContent = "";
+                        this.pokimon.board[this.pokimon.current -= 28].textContent = this.pokimon.headSymbol;
                     }
                     else {
                         clearInterval(interval)
@@ -89,9 +80,9 @@ class Pokimon {
                     };
                 }
                 else if (this.right) {
-                    if (!this.#maxRightBoarder(this.current)) {
-                        this.board[this.current].innerHTML = "";
-                        this.board[++this.current].innerHTML = this.headSymbol;
+                    if (!this.#maxRightBoarder(this.pokimon.current)) {
+                        this.pokimon.board[this.pokimon.current].textContent = "";
+                        this.pokimon.board[++this.pokimon.current].textContent = this.pokimon.headSymbol;
                     }
                     else {
                         clearInterval(interval)
@@ -99,19 +90,19 @@ class Pokimon {
                     };
                 }
                 else if (this.left) {
-                    if (!this.#maxLeftBoarder(this.current)) {
-                        this.board[this.current].innerHTML = "";
-                        this.board[--this.current].innerHTML = this.headSymbol;
+                    if (!this.#maxLeftBoarder(this.pokimon.current)) {
+                        this.pokimon.board[this.pokimon.current].textContent = "";
+                        this.pokimon.board[--this.pokimon.current].textContent = this.pokimon.headSymbol;
                     }
                     else {
                         clearInterval(interval)
                         this.endGame = true;
                     };
                 }
-                else {
-                    if (!this.#maxBottomBoarder(this.current)) {
-                        this.board[this.current].innerHTML = "";
-                        this.board[this.current += 28].innerHTML = this.headSymbol;
+                else if (this.down) {
+                    if (!this.#maxBottomBoarder(this.pokimon.current)) {
+                        this.pokimon.board[this.pokimon.current].textContent = "";
+                        this.pokimon.board[this.pokimon.current += 28].textContent = this.pokimon.headSymbol;
                     }
                     else {
                         clearInterval(interval)
@@ -121,36 +112,37 @@ class Pokimon {
             } catch (error) {
                 clearInterval(interval);
                 this.endGame = true;
-                console.log(new Error(error));
+                console.log(error);
             }
-        }, this.untiSpeed);
+        }, this.pokimon.untiSpeed);
     }
 
+    //to detect movement direction, if same range go right or left, otherwise go up or down
     IMoveTo(index = undefined) {
         if (index === undefined) {
             return;
         }
-        if (this.#checkRange(index, this.current)) {
-            if (this.current < index) {
+        if (this.#checkRange(index, this.pokimon.current)) {
+            if (this.pokimon.current < index) {
                 this.up = false;
-                this.right = true;
                 this.down = false;
+                this.right = true;
                 this.left = false;
             }
-            else if (this.current > index) {
+            else if (this.pokimon.current > index) {
                 this.up = false;
                 this.right = false;
                 this.down = false;
                 this.left = true;
             }
         }
-        else if (this.current > index) {
+        else if (this.pokimon.current > index) {
             this.up = true;
             this.right = false;
             this.down = false;
             this.left = false;
         }
-        else if (this.current < index) {
+        else if (this.pokimon.current < index) {
             this.up = false;
             this.right = false;
             this.down = true;
@@ -158,15 +150,42 @@ class Pokimon {
         }
     }
 
+    //to check wither the apple is in my range or not, so i know where to move
+    #ranges = [[0, 27], [28, 55], [56, 83], [84, 111], [112, 139], [140, 167], [168, 195], [196, 223], [224, 251],
+    [252, 279], [280, 307], [308, 335], [336, 363], [364, 391], [392, 419], [420, 447], [448, 475], [476, 503],
+    [504, 531], [532, 559]];
+}
+
+class FoodManager {
+    //to generate a random index so that put an Apple into it
+    #getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    //to check if there is any food inside the board, to avoid adding more food
+    #checkFood() {
+        for (let element of this.pokimon.board) {
+            if (element.textContent !== "" && element.textContent !== `${this.pokimon.headSymbol}`) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    constructor(pokimon, movementManager) {
+        this.pokimon = pokimon;
+        this.movementManager = movementManager;
+    }
+
+    //to initiate food
     Ifood() {
         let interval = setInterval(() => {
-            if (!this.#checkFood(this.board)) {
+            if (!this.#checkFood(this.pokimon.board)) {
                 let rand = this.#getRandomInt(0, 559);
-                this.board[rand].textContent = "🍎";
-                this.score++;
-                this.untiSpeed -= 100;
+                this.pokimon.board[rand].textContent = "🍎";
+                this.pokimon.score++;
             }
-            if (this.endGame) {
+            if (this.movementManager.endGame) {
                 clearInterval(interval);
             }
         }, 200);
@@ -175,22 +194,29 @@ class Pokimon {
 
 // initialize a snake
 let cells = document.querySelectorAll(".cell");
-let sk1 = new Pokimon(cells, 350, '🤪', '⚫', 250);
+let pk1 = new Pokimon(cells, 0, '🤪', 200);
+let mManager = new MovementManager(pk1);
+let fManager = new FoodManager(pk1, mManager);
 let index = undefined;
-sk1.IMove();
-sk1.Ifood();
+mManager.IMove();
+fManager.Ifood();
 
-setInterval(() => {
-    if (index === sk1.current) {
+let interval = setInterval(() => {
+    if (index === pk1.current) {
         index = undefined;
     }
-    sk1.IMoveTo(index);
-}, 100);
+    if (mManager.endGame) {
+        clearInterval(interval);
+    }
+    mManager.IMoveTo(index);
+}, pk1.untiSpeed);
 
 //move on click functionality
-let interval;
 cells.forEach((e) => {
     e.addEventListener('click', () => {
         index = e.dataset.index - 1;
     });
 });
+
+
+//note : don't forget the pause functionality, it is done when all directions are false;
